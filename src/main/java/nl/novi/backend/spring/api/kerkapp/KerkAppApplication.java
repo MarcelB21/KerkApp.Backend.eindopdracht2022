57ae322e0d950710cf2f6dbbf31d2709cd0308ba
MarcelB21 <marcelbrzezinski@msn.com>
2022-06-28 16:13:09 +0200

    first commit

---
+++ src/main/java/nl/novi/backend/spring/api/kerkapp/KerkAppApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.backend.spring.api.kerkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KerkAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KerkAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/backend/spring/api/kerkapp/KerkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.backend.spring.api.kerkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KerkAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
